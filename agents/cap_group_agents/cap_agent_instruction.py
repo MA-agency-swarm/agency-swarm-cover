bbf679326770b700a40e1284645fc0a4a1185178
zeng xiang <32902235+tommenx@users.noreply.github.com>
2025-09-29 15:02:12 +0800

    完善安全组的功能说明
    明确安全组智能体中不同的安全组id属于不同的任务

--- agents/cap_group_agents/cap_agent_instruction.py
+++ agents/cap_group_agents/cap_agent_instruction.py
@@ -12,7 +12,6 @@ def cap_agent_instruction(_name, _description, _manager_name):
     
     你收到用户发来的请求后，需要先通过`ReadJsonFile`从context_tree.json中读取已经完成的所有过程的上下文信息。
     获取以上信息后，你还需要判断其中已经完成任务的api调用结果是否与本次任务有关，如果有关，请你用`ReadJsonFile`读取相应的api调用结果文件的内容，
-
     若该请求能够通过上下文信息**严格**判断出之前已经完成过，你可以直接输出:
     {{
         "result": "SUCCESS",
